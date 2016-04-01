0179b33fb4e4453872413fa219cdb19b42dd802b
Shivam38 <shivam.mishra1@huawei.com>
2016-04-01 18:33:11 +0530

    updated

--- mesos_fed.sh
+++ mesos_fed.sh
@@ -1,33 +1,24 @@
 #!/bin/bash
 
-ls
 x="$(date)"
+mkdir src
+cd src
 git clone https://git-wip-us.apache.org/repos/asf/mesos.git
 sudo apt-get update
 sudo apt-get install -y tar wget git
 sudo apt-get install -y openjdk-7-jdk
 sudo apt-get install -y autoconf libtool
 sudo apt-get -y install build-essential python-dev python-boto libcurl4-nss-dev libsasl2-dev libsasl2-modules maven libapr1-dev libsvn-dev
-echo "going under mesos dir..."
-sleep 1m
 cd mesos
-echo "running bootstrap..."
-sleep 1m
 ./bootstrap
 mkdir build
 cd build
 ../configure
 echo "going to run make..."
-sudo make
+sudo make -j 8
 echo "going to run make check..."
-sudo make check
+sudo make check -j 8
 echo "going to run make install..."
-sudo make install
+sudo make install -j 8
 date
 echo "$x"
-#including FedModules
-
-#cd ../../
-#git clone https://github.com/huawei-cloudfederation/FedModules.git
-#cd FedModules
-#./build.sh
